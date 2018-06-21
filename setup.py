585053990845121cce911452f175629fc48d6010
stdavis <stdavis@utah.gov>
2018-06-21 09:52:57 -0600

    v1.3.0 Changelog and version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="honeycomb",
-    version="1.2.0",
+    version="1.3.0",
     license="MIT",
     description="CLI tool for managing AGRC base map caches (vector & raster).",
     long_description="",
