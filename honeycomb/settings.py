bcb6c3c3749d6f253f74182eb6032e1ea10ac7e7
Scott Davis <stdavis@utah.gov>
2022-02-01 11:23:28 -0700

    fix: update cache directory location

--- honeycomb/settings.py
+++ honeycomb/settings.py
@@ -13,7 +13,7 @@ if SHARE is None:
     raise Exception('Please set the "{}" environmental variable!'.format(SHARE_ENV_NAME))
 PREVIEW_URL = r'http://{}/arcgis/rest/services/{}/MapServer?f=jsapi'
 EXTENTSFGDB = os.path.join(os.path.dirname(__file__), 'data', 'Extents.gdb')
-CACHE_DIR = r'C:\arcgisserver\directories\arcgiscache'
+CACHE_DIR = r'E:\arcgisserver\directories\arcgiscache'
 TEST_EXTENT = os.path.join(EXTENTSFGDB, 'test_extent')
 
 COMPLETE_NUM_BUNDLES_LU = {
