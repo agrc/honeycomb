a5c12f0f2e96cca7b54bf9b953a639198162d9df
AGRC Dev <agrc-dev@utah.gov>
2023-07-31 23:26:45 +0000

    fix: enable wait script logging to gcp

--- scripts/machine_startup.bat
+++ scripts/machine_startup.bat
@@ -1,4 +1,3 @@
-call propy C:\dev\honeycomb\scripts\wait_for_license.py
-call propy C:\dev\honeycomb\scripts\wait_for_server.py
 call activate honeycomb
+call python C:\dev\honeycomb\scripts\wait_for_license_and_server.py
 call honeycomb resume
