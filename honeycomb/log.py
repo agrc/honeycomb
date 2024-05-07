c3c575eeeb86b001cb41265ba84cf9d243566400
AGRC Dev <agrc-dev@utah.gov>
2024-05-07 13:36:38 +0000

    feat: add global exception log handler

--- honeycomb/log.py
+++ honeycomb/log.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+from os import linesep
 
 import google.cloud.logging
 from tqdm import tqdm
@@ -11,6 +13,9 @@ def init():
     logger.setLevel(logging.INFO)
     client = google.cloud.logging.Client()
     client.setup_logging()
+
+    #: add global exception handlers
+    sys.excepthook = global_exception_handler
 
 
 #: https://github.com/tqdm/tqdm/issues/313#issuecomment-812224667
@@ -40,3 +45,22 @@ class logging_tqdm(tqdm):
             return
 
         self.logger.info("%s", msg)
+
+
+def global_exception_handler(ex_cls, ex, tb):
+    """
+    ex_cls: Class - the type of the exception
+    ex: object - the exception object
+    tb: Traceback
+
+    Used to handle any uncaught exceptions. Formats an error message and logs it.
+    """
+    import traceback
+
+    last_traceback = (traceback.extract_tb(tb))[-1]
+    line_number = last_traceback[1]
+    file_name = last_traceback[0].split(".")[0]
+    error = linesep.join(traceback.format_exception(ex_cls, ex, tb))
+
+    logger.error(("global error handler line: %s (%s)" % (line_number, file_name)))
+    logger.error(error)
