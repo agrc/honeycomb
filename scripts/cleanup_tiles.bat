fcc03fe3c88a6273beffb735843836ad501a4f16
AGRC Dev <agrc-dev@utah.gov>
2024-08-16 13:33:50 +0000

    feat: add script for cleaning up cache tiles manually

---
+++ scripts/cleanup_tiles.bat
@@ -0,0 +1,22 @@
+@REM Clean up all tiles for a particular cache
+
+echo off
+if "%1" == "" goto usage
+
+set folder="C:\arcgisserver\directories\arcgiscache\%1\%1\_alllayers"
+echo Deleting all files and subdirectories in %folder%
+
+for /d %%x in (%folder%\*) do (
+  echo Deleting: %%x
+  del /f/q/s %%x > nul
+  rmdir /q/s %%x
+)
+
+goto end
+
+:usage
+echo Usage: cleanup_tiles.bat ^<CacheName^>
+echo Example: cleanup_tiles.bat AddressPoints
+
+:end
+
