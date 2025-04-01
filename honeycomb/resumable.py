8ce9b303e88732dfb8ceeabb9c96cd5c7fd06ccd
AGRC Dev <agrc-dev@utah.gov>
2025-04-01 08:10:24 -0600

    chore: add types to resumable

--- honeycomb/resumable.py
+++ honeycomb/resumable.py
@@ -1,18 +1,37 @@
 import json
 import os
 from pathlib import Path
+from typing import List, Literal, Optional, TypedDict, Union
 
 from .config import config_folder
 
 file_path = Path(config_folder) / "current_job.json"
+Properties = Literal[
+    "data_updated",
+    "test_cache_complete",
+    "cache_extents_completed",
+    "caching_complete",
+    "exploding_complete",
+    "restart_times",
+]
 
 
-def cache_job_status(job):
+class Job(TypedDict):
+    cache_args: List[Union[str, bool, List[int]]]
+    data_updated: bool
+    test_cache_complete: bool
+    cache_extents_completed: List[str]
+    caching_complete: bool
+    exploding_complete: bool
+    restart_times: List[str]
+
+
+def cache_job_status(job: Job) -> None:
     with open(file_path, "w") as file:
         json.dump(job, file, indent=2)
 
 
-def get_current_job():
+def get_current_job() -> Optional[Job]:
     try:
         with open(file_path, "r") as file:
             return json.load(file)
@@ -20,8 +39,15 @@ def get_current_job():
         return None
 
 
-def start_new_job(basemap, missing_only, skip_update, skip_test, spot, levels):
-    job = {
+def start_new_job(
+    basemap: str,
+    missing_only: bool,
+    skip_update: bool,
+    skip_test: bool,
+    spot: str,
+    levels: List[int],
+) -> None:
+    job: Job = {
         "cache_args": [basemap, missing_only, skip_update, skip_test, spot, levels],
         "data_updated": False,
         "test_cache_complete": False,
@@ -34,13 +60,17 @@ def start_new_job(basemap, missing_only, skip_update, skip_test, spot, levels):
     cache_job_status(job)
 
 
-def update_job(prop, value):
-    job = get_current_job()
+def update_job(
+    prop: Properties,
+    value: Union[str, bool],
+) -> None:
+    job: Optional[Job] = get_current_job()
 
     if job is None:
         raise Exception("No job has been created!")
 
     try:
+        #: if the property is a list, append the value to it
         job[prop].append(value)
     except AttributeError:
         job[prop] = value
@@ -48,7 +78,7 @@ def update_job(prop, value):
     cache_job_status(job)
 
 
-def get_job_status(prop):
+def get_job_status(prop: Properties) -> Union[str, bool, List[str]]:
     job = get_current_job()
 
     if job is None:
@@ -57,5 +87,5 @@ def get_job_status(prop):
     return job[prop]
 
 
-def finish_job():
+def finish_job() -> None:
     os.remove(file_path)
